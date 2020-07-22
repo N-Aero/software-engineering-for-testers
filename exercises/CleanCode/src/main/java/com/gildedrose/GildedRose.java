da7c7cb3f3cb42082b4f573e17cf2c2bda327d52
Yuri Verweij <yuri@yufra.nl>
2020-07-22 06:42:09 +0200

    refactoring

--- exercises/CleanCode/src/main/java/com/gildedrose/GildedRose.java
+++ exercises/CleanCode/src/main/java/com/gildedrose/GildedRose.java
@@ -3,21 +3,22 @@ package com.gildedrose;
 import com.gildedrose.ProductTypes.Product;
 
 class GildedRose {
+    private ProductFactory productFactory;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
+        this.productFactory = new ProductFactory();
         this.items = items;
     }
 
-    private Product product(Item item) {
-        return new ProductFactory().getProduct(item);
-    }
 
     public void updateProducts() {
 
         //get an object of Circle and call its draw method.
         for (Item item : items) {
-            product(item).updatePriceAndSellIn(item);
+            Product product = productFactory.getProduct(item);
+            product.updatePriceAndSellIn(item);
         }
     }
 }
